90fef577c03321acfd5f824381b67574748c606a
misrepresenting <64040894+misrepresenting@users.noreply.github.com>
2020-12-22 20:58:07 -0500

    Create ParentGui.lua

---
+++ Modules/ParentGui.lua
@@ -0,0 +1,17 @@
+return function(Gui)
+	Gui.Name = HTTPService:GenerateGUID(false):gsub("-",""):sub(1,math.random(25,30))
+
+	if ((not is_sirhurt_closure) and (syn and syn.protect_gui)) then
+		syn.protect_gui(Gui)
+		Gui.Parent = CoreGui
+	elseif (get_hidden_gui or gethui) then
+		local HiddenUI = get_hidden_gui or gethui
+		Gui.Parent = HiddenUI
+	elseif (CoreGui:FindFirstChild("RobloxGui")) then
+		Gui.Parent = CoreGui.RobloxGui
+	else
+		Gui.Parent = CoreGui
+	end
+
+	return Gui
+end
